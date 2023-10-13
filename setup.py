8b8dec7bf3542603e17962be53b738e7c771dc9d
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2023-10-13 19:01:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup (
     name = "MORALS",
     version = "0.1.1",
-    author = "Aravind Sivaramakrishnan, Dhruv Metha Ramesh, Ewerton Rocha Vieira",
+    author = "Aravind Sivaramakrishnan, Ewerton Rocha Vieira, Sumanth Tangirala",
     url = "https://github.com/Ewerton-Vieira/MORALS.git",
     description = "MORALS: Morse Graph-aided discovery of Regions of Attraction in a learned Latent Space",
     long_description = open('README.md').read(),
     ext_package='MORALS',
     packages=['MORALS'],
-    install_requires = ['numpy', 'scipy', 'matplotlib', 'scikit-learn', 'pandas', 'seaborn', 'tqdm', 'torch', 'torchvision', 'wandb', 'CMGDB', 'dytop'],
+    install_requires = ['numpy', 'scipy', 'matplotlib', 'scikit-learn', 'pandas', 'seaborn', 'tqdm', 'torch', 'torchvision', 'CMGDB', 'dytop'],
 )
